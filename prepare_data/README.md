3d41270df29a89835df45162c8241909b40922d2
何炼 <lian.he@bitmain.com>
2019-08-19 14:29:19 +0800

    update showbbx.py

---
+++ prepare_data/README.md
@@ -0,0 +1,6 @@
+wider face data can be download in this website:  
+http://shuoyang1213.me/WIDERFACE/
+
+script descript:  
+1. showbbx.py 
+use to show the bbx on image
---
+++ prepare_data/showbbx.py
@@ -0,0 +1,36 @@
+import cv2
+import os
+
+wild_root = '/home/lianhe/WIDER_train/images'
+anno_txt = '/home/lianhe/wider_face_split/wider_face_train_bbx_gt.txt'
+
+last_file=""
+last_rect=[]
+with open(anno_txt,"r") as f:
+    for each in f.readlines():
+        each = each.rstrip()
+        if each.find("jpg")!=-1:
+          if each != last_file and last_file != "":
+              image_path = os.path.join(wild_root,last_file)
+              img = cv2.imread(image_path,1)
+#              print image_path
+#              print img
+              for rect in last_rect:
+                  print rect
+                  [x1, y1, w, h, blur, expression, illumination, invalid, occlusion, pose] = rect
+                  #print (x1,y1),(x1+w,y1+h)
+                  cv2.rectangle(img,(x1,y1),(x1+w,y1+h),(255,0,0),2)
+              cv2.imshow("img",img)
+              cv2.waitKey(0)
+#              break;
+          last_file = each
+          last_rect = []
+        else:
+          try:
+            mlist = each.split()
+            if len(mlist) > 2:
+              mlist = [int(i) for i in mlist]
+              print mlist
+              last_rect.append(mlist)
+          except:
+            pass
